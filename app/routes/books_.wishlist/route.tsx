a1e7ac24b7c3b6feae7ff4e42e54bc47a41eec4f
debojyotighosh <contact@debojyotighosh.com>
2025-03-05 22:20:43 +0530

    Minor fix.

--- app/routes/books_.wishlist/route.tsx
+++ app/routes/books_.wishlist/route.tsx
@@ -1,9 +1,11 @@
-// `books_` folder ensures that the layout is not nested while still keeping the URL nesting pattern
-
 const BooksWaitlist = () => {
   return (
     <div>
       <h1>Book wishlist</h1>
+      <p>
+        <span className="px-1 border border-red-400 bg-red-950 rounded">books_</span> folder
+        ensures that the layout is not nested, while still keeping the URL nesting pattern
+      </p>
     </div>
   )
 }
