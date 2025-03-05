1bd8c7689561a3f1fc5350b329f7c8cf2989fe23
debojyotighosh <contact@debojyotighosh.com>
2025-03-05 19:13:15 +0530

    route.tsx vs index.tsx

---
+++ app/routes/lego/route.tsx
@@ -0,0 +1,10 @@
+const LegoRoute = () => {
+  return (
+    <div>
+      <h1>The route.tsx page for Lego</h1>
+      <p>This file is treated as default inside a folder</p>
+    </div>
+  )
+}
+
+export default LegoRoute
