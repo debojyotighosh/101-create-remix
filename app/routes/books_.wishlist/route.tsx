323d60e9fbfc84fdde1b568c99c39e7995ee6ba5
debojyotighosh <contact@debojyotighosh.com>
2025-03-05 22:07:51 +0530

    Nested URLs without layout nesting.

---
+++ app/routes/books_.wishlist/route.tsx
@@ -0,0 +1,11 @@
+// `books_` folder ensures that the layout is not nested while still keeping the URL nesting pattern
+
+const BooksWaitlist = () => {
+  return (
+    <div>
+      <h1>Book wishlist</h1>
+    </div>
+  )
+}
+
+export default BooksWaitlist
